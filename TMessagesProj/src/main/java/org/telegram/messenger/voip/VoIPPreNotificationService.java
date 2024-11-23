88b217994606fbca87f7c33a0d20a592df59dcb0
andfed <andrzej.p198002@gmail.com>
2024-11-23 16:52:57 +0100

    Fix vibration for SDK >= 33

--- TMessagesProj/src/main/java/org/telegram/messenger/voip/VoIPPreNotificationService.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/voip/VoIPPreNotificationService.java
@@ -18,6 +18,8 @@ import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.VibrationAttributes;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.text.SpannableString;
@@ -361,11 +363,22 @@ public class VoIPPreNotificationService { // } extends Service implements AudioM
                     } else if (vibrate == 3) {
                         duration *= 2;
                     }
-                    AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                            .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
-                            .build();
-                    vibrator.vibrate(new long[]{0, duration, 500}, 0, audioAttributes);
+                    long[] pattern = new long[]{0, duration, 500};
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        vibrator.vibrate(
+                            VibrationEffect.createWaveform(pattern, 0),
+                            new VibrationAttributes.Builder()
+                                .setUsage(VibrationAttributes.USAGE_RINGTONE) // required for background apps
+                                .build());
+                    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                                .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                                .build();
+                        vibrator.vibrate(pattern, 0, audioAttributes);
+                    } else {
+                        vibrator.vibrate(pattern, 0);
+                    }
                 }
             }
         }
