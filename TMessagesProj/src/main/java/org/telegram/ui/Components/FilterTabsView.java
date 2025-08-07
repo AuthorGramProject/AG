3f230561cadeaf966ac9cffc9a2e1f379e6a6368
Keeperorowner <chenhai0731@gmail.com>
2025-08-08 01:14:40 +0800

    fix: Pill position error when closing iOS search panel

--- TMessagesProj/src/main/java/org/telegram/ui/Components/FilterTabsView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/FilterTabsView.java
@@ -1539,10 +1539,15 @@ public class FilterTabsView extends FrameLayout {
                 if (customTabStyle >= TabStyle.PILLS.getValue()) {
                     inlinePadding = AndroidUtilities.dp(10);
                     int searchPanelHeight = getTabsHeight();
-                    int tabAreaHeight = height - searchPanelHeight;
-                    int tabCenterY = searchPanelHeight + tabAreaHeight / 2 + AndroidUtilities.dp(10);
-                    topBound = tabCenterY - AndroidUtilities.dp(15);
-                    bottomBound = tabCenterY + AndroidUtilities.dp(15);
+                    if (searchPanelHeight > 0) {
+                        int tabAreaHeight = height - searchPanelHeight;
+                        int tabCenterY = searchPanelHeight + tabAreaHeight / 2 + AndroidUtilities.dp(10);
+                        topBound = tabCenterY - AndroidUtilities.dp(15);
+                        bottomBound = tabCenterY + AndroidUtilities.dp(15);
+                    } else {
+                        topBound = height / 2 - AndroidUtilities.dp(15);
+                        bottomBound = height / 2 + AndroidUtilities.dp(15);
+                    }
                     selectorDrawable.setColor(ColorUtils.setAlphaComponent(Theme.getColor(tabLineColorKey), 50));
                 } else {
                     selectorDrawable.setColor(Theme.getColor(tabLineColorKey));
