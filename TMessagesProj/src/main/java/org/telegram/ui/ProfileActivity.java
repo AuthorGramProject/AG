60f410c298eb265bbcd3681c20555a4f64d8956a
A <vadym@MIOs-iMac.fritz.box>
2026-08-30 08:16:05 +0200

    fix: Sync badge revert, changelog removal, call button fix from agp

--- TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
@@ -11530,7 +11530,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
     // AuthorGram: метод для отримання бейджа розробника
     private Drawable getAuthorBadgeDrawable() {
         if (authorBadgeDrawable == null) {
-            authorBadgeDrawable = new org.telegram.messenger.authorgram.AuthorGramBadgeDrawable();
+            authorBadgeDrawable = org.telegram.messenger.ApplicationLoader.applicationContext.getResources().getDrawable(org.telegram.messenger.R.drawable.ic_author_badge).mutate();
         }
         return authorBadgeDrawable;
     }
