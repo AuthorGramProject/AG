a7d60629e222d4887c1b2976ab220970d569e22a
AuthorChe | Vadym_Yem <81570039+VadymYem@users.noreply.github.com>
2026-09-04 17:08:30 +0200

    fix: complete Play Billing Library 8 migration

--- TMessagesProj/src/main/java/org/telegram/messenger/BillingController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BillingController.java
@@ -18,8 +18,8 @@ import com.android.billingclient.api.BillingClientStateListener;
 import com.android.billingclient.api.BillingFlowParams;
 import com.android.billingclient.api.BillingResult;
 import com.android.billingclient.api.ConsumeParams;
+import com.android.billingclient.api.PendingPurchasesParams;
 import com.android.billingclient.api.ProductDetails;
-import com.android.billingclient.api.ProductDetailsResponseListener;
 import com.android.billingclient.api.Purchase;
 import com.android.billingclient.api.PurchasesResponseListener;
 import com.android.billingclient.api.PurchasesUpdatedListener;
@@ -79,7 +79,11 @@ public class BillingController implements PurchasesUpdatedListener, BillingClien
 
     private BillingController(Context ctx) {
         billingClient = BillingClient.newBuilder(ctx)
-                .enablePendingPurchases()
+                .enablePendingPurchases(
+                        PendingPurchasesParams.newBuilder()
+                                .enableOneTimeProducts()
+                                .build()
+                )
                 .setListener(this)
                 .build();
     }
@@ -175,11 +179,22 @@ public class BillingController implements PurchasesUpdatedListener, BillingClien
         return billingClient.isReady();
     }
 
-    public void queryProductDetails(List<QueryProductDetailsParams.Product> products, ProductDetailsResponseListener responseListener) {
+    @FunctionalInterface
+    public interface ProductDetailsResponseCallback {
+        void onResponse(BillingResult billingResult, List<ProductDetails> productDetailsList);
+    }
+
+    public void queryProductDetails(List<QueryProductDetailsParams.Product> products, ProductDetailsResponseCallback responseListener) {
         if (!isReady()) {
             throw new IllegalStateException("Billing: Controller should be ready for this call!");
         }
-        billingClient.queryProductDetailsAsync(QueryProductDetailsParams.newBuilder().setProductList(products).build(), responseListener);
+        billingClient.queryProductDetailsAsync(
+                QueryProductDetailsParams.newBuilder().setProductList(products).build(),
+                (billingResult, result) -> responseListener.onResponse(
+                        billingResult,
+                        result == null ? Collections.emptyList() : result.getProductDetailsList()
+                )
+        );
     }
 
     /**
